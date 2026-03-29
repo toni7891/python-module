1ce38a7c9196c7753d417d774a98a940a71294da
toni7891 <toniv7891@gmail.com>
2026-03-29 11:44:22 +0300

    added loops

---
+++ intro/floop.py
@@ -0,0 +1,55 @@
+def main():
+    
+    # for i in range(1,6):
+    #     print(i)
+    
+    # for i in range(5,0,-1):
+    #     print(i)
+    
+    # for num in range(2,11,2):
+    #     print(num)
+        
+    # for num in range(1,11,1):
+    #     print(num)
+    
+    # for i in range(1,20):
+    #     if i % 3 == 0:
+    #         print(i)
+    
+    # for i in range(5):
+    #     print("Hello")
+    
+    # for i in range(2,22,2):
+    #     print(i)
+    
+    # for i in range(10,-1,-1):
+    #     print(i)
+    
+    # sum=0
+    
+    # for i in range(1,101,1):
+    #     sum += i
+    # print(sum)
+    
+    # names = ["alice","bob","ron","philip"]
+    # counter = 0
+    # for i in names:
+    #     counter += 1
+    #     print(f"hello, {i}!")
+    
+    # print("num of names ", counter)
+    # print("num of names ", len(names))
+    
+    numbers = [234,5,2,1,231,4,5,54]
+    sum = 0
+    for num in numbers:
+        sum += num
+    print(sum)    
+    numbers.sort()
+    print(numbers[-1])
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ intro/wloop.py
@@ -0,0 +1,41 @@
+def main():
+    # sum = 0
+    # i = 1
+    # while i < 6:
+    #     sum += i
+    #     print(i)
+    #     i += 1
+        
+    # n = 6
+    # while n != 1 :
+    #     n -= 1 
+    #     sum += n
+    #     print(n)
+    
+    # print(sum)
+    
+    # user = input("enter a number: ")
+    # ind = int(user)
+    # while ind < 5:
+    #     print(user)
+    #     ind += 1
+    
+    
+    # i=1
+    # while i <= ind:
+    #     print(i)
+    #     i+=1
+    
+    answer = 1
+    # while answer != 0:
+    #     answer = int(input("enter 0 to stop: "))
+   
+   
+    while answer != "1234" :
+        answer = input("enter a password: ") 
+       
+    
+    
+
+if __name__ == "__main__":
+    main()
